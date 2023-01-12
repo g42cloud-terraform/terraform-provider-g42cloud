ff0360b5d8764206f0d8f7396d1474e9f81fa42f
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2023-01-12 07:05:20 +0000

    Clean up the ha web example

---
+++ examples/high-available-web/ecs.tf
@@ -0,0 +1,27 @@
+# Fetching the availability zones of the region
+data "g42cloud_availability_zones" "az" {}
+
+# Feching the images with the specificed filters
+data "g42cloud_images_image" "image" {
+  name = "CentOS 7.9 64bit"
+  visibility = "public"
+  most_recent = true
+}
+
+# Create 2 ecs instances in different availability zones
+resource "g42cloud_compute_instance" "ecs" {
+  count = 2
+  availability_zone  = data.g42cloud_availability_zones.az.names[count.index]
+  security_group_ids = [g42cloud_networking_secgroup.secgroup.id]
+  system_disk_size   = var.ecs_volume_size
+  system_disk_type   = var.ecs_volume_type
+
+  name       = "${var.ecs_name}_${count.index+1}"
+  flavor_id  = var.ecs_flavor_id
+  image_id   = data.g42cloud_images_image.image.id
+  admin_pass = var.ecs_password
+
+  network {
+    uuid = data.g42cloud_vpc_subnet.subnet.id
+  }
+}
---
+++ examples/high-available-web/elb.tf
@@ -0,0 +1,56 @@
+resource "g42cloud_lb_loadbalancer" "elb" {
+  name          = var.elb_name
+  vip_subnet_id = data.g42cloud_vpc_subnet.subnet.subnet_id
+}
+
+resource "g42cloud_lb_listener" "listener_http" {
+  protocol = "HTTP"
+  protocol_port = "80"
+  loadbalancer_id = g42cloud_lb_loadbalancer.elb.id
+}
+
+resource "g42cloud_lb_member" "member" {
+  count = 2
+  address = g42cloud_compute_instance.ecs[count.index].access_ip_v4
+  pool_id = g42cloud_lb_pool.group_http.id
+  protocol_port = var.elb_listen_port
+  subnet_id = data.g42cloud_vpc_subnet.subnet.subnet_id
+  weight = 1
+}
+
+resource "g42cloud_lb_monitor" "monitor_http" {
+  delay = 5
+  max_retries = 3
+  pool_id = "${g42cloud_lb_pool.group_http.id}"
+  timeout = 3
+  type = "HTTP"
+  url_path = "/"
+}
+
+resource "g42cloud_lb_pool" "group_http" {
+  lb_method = "ROUND_ROBIN"
+  listener_id = g42cloud_lb_listener.listener_http.id
+  name = "group_http"
+  protocol = "HTTP"
+}
+
+# Create eip for the loadbalancer
+resource "g42cloud_vpc_eip" "eip" {
+  name = "${var.elb_name}-eip"
+  bandwidth {
+    name        = "${var.elb_name}-bandwidth"
+    size        = var.eip_bandwidth_size
+    charge_mode = "bandwidth"
+    share_type  = "PER"
+  }
+
+  publicip {
+    type = "5_bgp"
+  }
+}
+
+# Bind the above eip to the specified loadbalancer
+resource "g42cloud_vpc_eip_associate" "eip" {
+  port_id = g42cloud_lb_loadbalancer.elb.vip_port_id
+  public_ip = g42cloud_vpc_eip.eip.address
+}
