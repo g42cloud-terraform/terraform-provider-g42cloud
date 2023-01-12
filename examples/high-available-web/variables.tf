1fef032bf908bde095de6e95a754d877b98d3223
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2023-01-12 11:58:46 +0000

    Add eps support to examples

--- examples/high-available-web/variables.tf
+++ examples/high-available-web/variables.tf
@@ -1,3 +1,8 @@
+variable "eps_id" {
+  description = "The id of the enterprise project"
+  default = "fc04bc67-2b65-4898-ab71-ce0d63fbc267"
+}
+
 variable "security_group_name" {
   description = "The name of the security group"
   default = "high-available-web-demo"
@@ -36,7 +41,7 @@ variable "rds_name" {
 
 variable "rds_flavor_id" {
   description = "The flavor id used to create the rds instance"
-  default = "rds.mysql.m6.xlarge.8.ha"
+  default = "rds.mysql.c6.large.2.ha"
 }
 
 variable "rds_volume_size" {
